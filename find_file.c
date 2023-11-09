9907d53744c950ddf2e8ce55a9b664a8bfda0f41
Therese-Claire <awonguagabi@gmail.com>
2023-11-09 14:49:58 -0800

    function that handles PATH

---
+++ find_file.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+
+/**
+ * find_command - Function finds a file in the PATH variable
+ * @filename: Name of the fie to find
+ *
+ * Return: the full file PATH if it exist. Otherwise NULL.
+ */
+char *find_command(const char *filename)
+{
+	char *path, *token;
+	char *path_copy;
+
+	if (!filename)
+		return (NULL);
+
+	if (filename[0] == '/')
+	{
+		if (access(filename, F_OK) == 0)
+		{
+			return (_strdup(filename));
+
+		}
+		else
+		{
+			perror("./shell");
+			return (NULL);
+		}
+	}
+
+	path = getenv("PATH");
+	path_copy = _strdup(path);
+
+	token = strtok(path_copy, ":");
+
+	while (token != NULL)
+	{
+		char filepath[1024];
+
+		snprintf(filepath, sizeof(filepath), "%s/%s", token, filename);
+
+		if (access(filepath, F_OK) == 0)
+		{
+			free(path_copy);
+			return (_strdup(filepath));
+		}
+		token = strtok(NULL, ":");
+
+	}
+	free(path_copy);
+	return (NULL);
+}
