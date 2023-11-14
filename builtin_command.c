58389289634bfbf9045b10f65f43bcf19b86516a
Darius Tohtin <dariustohtin@gmail.com>
2023-11-14 15:29:04 -0800

    edited

--- builtin_command.c
+++ builtin_command.c
@@ -1,33 +1,23 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
  * exit_shell - function to terminate the program
  *
  * @command: command to be executed
  *
  * Return: void
-=======
- * exit_Shell - function to terminate the program.
- * @command: Double pointer to the array of commands
->>>>>>> 2eb0d84bceca5d7d0f2c14f81f8f48668b70d7aa
  */
-void exit_Shell(char **command)
+void exit_shell(char **command)
 {
 	free_command(command);
 	exit(EXIT_SUCCESS);
 }
 /**
-<<<<<<< HEAD
  * print_env - built in function to print environment
  *
  * @command: command to be executed
  *
  * Return: void
-=======
- * print_env - built in function to print environment.
- * @command: Double pointer to the array of commands
->>>>>>> 2eb0d84bceca5d7d0f2c14f81f8f48668b70d7aa
  */
 void print_env(char **command)
 {
